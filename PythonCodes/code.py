b4dc6bbe8d7820e764521fbc15c07a9cca63e5ba
RekhuGopal <gopalrchinnarathod@gmail.com>
2023-07-22 10:47:37 +0000

    Update code.py

--- PythonCodes/code.py
+++ PythonCodes/code.py
@@ -3,15 +3,15 @@
 # import complex math module
 import cmath
 
-a = 1
+a_a = 1
 b = 5
 c = 6
 
 # calculate the discriminant
-d = (b**2) - (4*a*c)
+d = (b**2) - (4*a_a*c)
 
 # find two solutions
-sol1 = (-b-cmath.sqrt(d))/(2*a)
-sol2 = (-b+cmath.sqrt(d))/(2*a)
+sol1 = (-b-cmath.sqrt(d))/(2*a_a)
+sol2 = (-b+cmath.sqrt(d))/(2*a_a)
 
 print('The solution are {0} and {1}'.format(sol1,sol2))
